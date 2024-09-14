916226fbfb46d668fcbbdd8a96beae3a7588ca76
Abylkhair03 <nurbina@usf.edu>
2024-09-13 21:46:19 -0400

    https://github.com/nurbina1/Coding-Challenge-4.git

---
+++ inventory_management.js
@@ -0,0 +1,34 @@
+const inventory = [
+    {
+        name: 'Macbook',
+        price: 1500,
+        quantity: 2000,
+        lowStockLevel: 5
+    },
+    {
+        name: "Airpods",
+        price: 200,
+        quantity: 5000,
+        lowStockLevel: 10
+    },
+    {
+        name: "Ipad",
+        price: 999,
+        quantity: 2500,
+        lowStockLevel: 7
+    },
+    {
+        name: "AirTag",
+        price: 69,
+        quantity: 20000,
+        lowStockLevel: 4
+    },
+    {
+        name: "Iphone",
+        price: 1200,
+        quantity: 35000,
+        lowStockLevel: 10
+    }
+];
+// Commit message: "Initialize inventory with product object"
+
